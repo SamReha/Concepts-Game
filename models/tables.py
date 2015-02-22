d9be82fdbd8aee6300765ce8e18b30323887ec9b
Sam Reha <sreha@ucsc.edu>
2015-02-21 21:07:25 -0800

    Finished database structure for now

--- models/tables.py
+++ models/tables.py
@@ -1,17 +1,27 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
 
+# The player tables collects all the information relating to a particular player, including stats.
+# To find videos associated with a particular player, query the "videos" table with the player's ID.
+# caretakeID is the unique account ID of the caretake associated with a particular player.
 db.define_table('player',
+                Field('caretakerID'),
                 Field('name'),
-                Field('videoLinks'), # Video Links are represented as a JSON string list of complete youtube URLs.
                 Field('lastTimePlayed', 'datetime'),
                 Field('gamesPlayed'), # The number of games played
                 Field('percentCorrect'),
                 Field('totalTimePlayed'),
                )
 
+# Each entry in the videos table is a youtube link and a reference to the player that owns the video.
+db.define_table('videos',
+                Field('playerID'),
+                Field('link'),
+               )
+
+# Each entry in the problem table is a collection of information describing a particular problem.
 db.define_table('problem',
                 Field('problemType'), # "size", "fullness", "color", etc.
                 Field('complexity'), # "2", "3", etc. Number of objects to compare
-                Field('images'), # The set of images used in the problem
+                Field('images'), # The set of images used in the problem. Placeholder.
                )
